8979bd357cebb209420a02028f94dee6a59a7c45
Michael Deal <michael.deal@consensys.net>
2020-09-14 19:35:38 -0400

    Get ImageCycler component working

---
+++ src/ImageCycler.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+class ImageCycler extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            imageCycling: this.props.imageCycling,
+            speed: this.props.speed ? this.props.speed : 1.5,
+            imageSources: this.props.images,
+            imageIndex: 0
+        };
+    }
+
+    componentDidMount() {
+        if (this.state.imageCycling) this.cycleImages()
+    }
+
+    render() {
+        return (
+            <div>
+                <img id="testImage" src={this.state.imageSources[this.state.imageIndex]}></img>
+            </div>
+        );
+    };
+
+    cycleImages() {
+        this.state.imageIndex++
+        if (this.state.imageIndex == this.state.imageSources.length) {
+            this.setState({ imageIndex : 0 })
+        }
+        this.setState({ imageIndex: this.state.imageIndex++ })
+        setTimeout(this.cycleImages.bind(this), 1000 * this.state.speed )
+    }
+}
+
+export default ImageCycler;
--- src/Work.js
+++ src/Work.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link} from "react-router-dom";
-import CoverCycler from './CoverCycler';
-// import './covercycle.js'
+import CoverCycler from './ImageCycler';
 import './Work.scss';
 
 
 
-function Work() {
+class Work extends React.Component {
+
+    state = {
+        imageCycling: true,
+        vcbImageSources: [
+            "images/vcb/cover/vcb-cover-1.png",
+            "images/vcb/cover/vcb-cover-2.png",
+            "images/vcb/cover/vcb-cover-3.png",
+            "images/vcb/cover/vcb-cover-4.png",
+            "images/vcb/cover/vcb-cover-5.png",
+            "images/vcb/cover/vcb-cover-6.png",
+            "images/vcb/cover/vcb-cover-7.png",
+            "images/vcb/cover/vcb-cover-8.png",
+            "images/vcb/cover/vcb-cover-9.png"
+        ]
+    }
+
+    render() {
     return (
         <div className="Work">
             <section className="cover">
@@ -22,12 +38,13 @@ function Work() {
                     </div>
                 </div>
                 <div className="cover__body">
-                    {/* <img id="project-cover--vcb__image" src="images/vcb/cover/vcb-cover-1.png" alt="Verse Chorus Bridge cover"></img> */}
-                    <CoverCycler />
+                    <CoverCycler imageCycling={this.state.imageCycling} images={this.state.vcbImageSources} />
                 </div>
             </section>
         </div>
     );
+    }
+
 }
 
 export default Work;
