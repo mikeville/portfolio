9b3bc3c8d6e8033d362da8e8e7168e18550d7fb6
Michael Deal <michael.deal@consensys.net>
2020-09-15 14:54:48 -0400

    Got p5 working in React

--- src/CoverWhales.js
+++ src/CoverWhales.js
@@ -1,15 +1,38 @@
 import React from 'react';
+import p5 from 'p5';
 
 class CoverWhales extends React.Component {
     constructor(props) {
         super(props);
         this.state = {};
+        this.myRef = React.createRef();
+    }
+
+    sketch = function(p) {
+        console.log("test")
+        p.setup = function() {
+            p.noLoop();
+            console.log("setup ran")
+        }
+
+        p.draw = function() {
+            console.log("drawing");
+
+        }
+    }
+
+    componentDidMount() {
+        this.myP5 = new p5(this.sketch, this.myRef.current)
     }
 
     render() {
         return (
             <div className="CoverWhales">
-                Whale test here
+                <div className="cover__container">
+                    <div ref="{this.myRef}">
+
+                    </div>
+                </div>
             </div>
         );
     };
