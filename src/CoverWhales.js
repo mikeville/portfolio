42c458931448d7a26ead958ee8f4d4783df186d2
Michael Deal <michael.deal@consensys.net>
2020-09-15 15:21:25 -0400

    Testing p5 sketch. don't know why it's not rendering at bottom of page

--- src/CoverWhales.js
+++ src/CoverWhales.js
@@ -9,15 +9,62 @@ class CoverWhales extends React.Component {
     }
 
     sketch = function(p) {
-        console.log("test")
+        const canvas = 540;
+        let eyelid1Pos;
+        let eyelid2Pos;
+        
+        
         p.setup = function() {
             p.noLoop();
-            console.log("setup ran")
+            p.createCanvas(canvas, canvas);
+            p.colorMode(p.HSB, 100);
+            p.angleMode(p.DEGREES);
+            p.imageMode(p.CENTER);
         }
-
+        
+            
+        p.setEyelidPos = function() {
+        
+            if (p.mouseY < canvas*1/4) {
+                eyelid1Pos = canvas*1/4
+                eyelid2Pos = canvas*3/4
+            } else if (p.mouseY > canvas*1/4 && p.mouseY < canvas*2/4 ){
+                eyelid1Pos = p.mouseY
+                eyelid2Pos = canvas*2/4 - (p.mouseY - canvas*2/4)
+            } else if (p.mouseY > canvas*2/4 && p.mouseY < canvas*3/4){
+                eyelid1Pos = canvas*2/4 - (p.mouseY - canvas*2/4)
+                eyelid2Pos = p.mouseY
+            } else {
+                eyelid1Pos = canvas*3/4
+                eyelid2Pos = canvas*1/4
+            }
+        
+        }
+        
+        p.mouseMoved = function() {
+            p.setEyelidPos();
+            p.redraw();
+        }
+        
         p.draw = function() {
-            console.log("drawing");
-
+            p.background(80, 50, 80)
+        
+            p.setEyelidPos()
+        
+            p.noFill();
+            p.stroke(0, 0, 0, 80);
+            p.strokeWeight(2);
+        
+            p.beginShape();
+            p.vertex(0, canvas/2)
+            p.bezierVertex(canvas*(1/4), canvas*(2/4), canvas*(1/4), eyelid1Pos, canvas*(2/4), eyelid1Pos);
+            p.bezierVertex(canvas*(3/4), eyelid1Pos, canvas*(3/4), canvas*(1/2), canvas, canvas*(1/2));
+            p.bezierVertex(canvas*(3/4), canvas*(2/4), canvas*(3/4), eyelid2Pos, canvas*(1/2), eyelid2Pos);
+            p.bezierVertex(canvas*(1/4), eyelid2Pos, canvas*(1/4), canvas*(2/4), 0, canvas*(2/4));
+            p.endShape(p.CLOSE);
+        
+        
+        
         }
     }
 
