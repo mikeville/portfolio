2a5497e75a203cb6b117e399082e758060306eb5
Michael Deal <michael.deal@consensys.net>
2020-09-25 12:29:00 -0400

    Fix grid--3--hero sizing

--- src/work/Businessweek/Businessweek.js
+++ src/work/Businessweek/Businessweek.js
@@ -6,8 +6,19 @@ function Businessweek() {
     return (
       <div className="Project Businessweek">
         <ProjectHeader />
-        <div className="contained">
+        <div className="project__body contained">
           <h1 className="project__title">Businessweek</h1>
+          <h2 className="project__subtitle">Data visualization and design</h2>
+          <div className="project__intro columns columns--3--hero">
+            <div className="body-copy">
+              <p>
+                I joined the Businessweek team to lead the research and design of visual news stories for a special issue marking the five-year anniversary of the financial meltdown triggering the Great Recession.
+              </p>
+            </div>
+            <div>
+            test goes her and here and here. test goes her andsdf  here and here. test goes her and here and here. test goes her and here and here. test gasdf oes her and here and here. test goes her and here and here. test goes her and here and here. test goes her and here and here. test goes her and here and here. test goes her and here and here. test goes her and here andasdf  here. test goes her and here and here. 
+            </div>
+          </div>
         </div>
       </div>
     )
