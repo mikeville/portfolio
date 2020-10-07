6596db9757d541b2ea097a29b779f90df1635630
Michael Deal <michael.deal@consensys.net>
2020-10-07 17:25:57 -0400

    Port Rapmatic content

--- src/work/Businessweek/Businessweek.js
+++ src/work/Businessweek/Businessweek.js
@@ -12,70 +12,74 @@ function Businessweek() {
       <div className="Project Businessweek">
         <ProjectHeader />
         <div className="project__body contained">
-          <h1 className="project__title">Businessweek</h1>
-          <h2 className="project__subtitle">Data visualization and design</h2>
-          <section className="grid project__intro">
-            <div className="span-7 body-copy">
-              <p>
-                I joined the Businessweek team to lead the research and design of visual news stories for a special issue marking the five-year anniversary of the financial meltdown triggering the Great Recession.
-              </p>
-            </div>
-            <div className="span-3 align-right note">
-              <p className="note__title">
-                Team
-              </p>
-              <ul className="credit-list">
-                <li>Editor: Josh Tyrangiel</li>  
-                <li>Creative Director: Richard Turley</li>  
-                <li>Design Director: Cynthia Hoffman</li>  
-                <li>Designers and graphics editors: Rob Vargas, Allison McCann, Dorothy Gambrell, Chris Nosenzo, &amp; Evan Applegate.</li>  
-              </ul>
-            </div>
-          </section>
-          <section>
-            <h3 className="project__section-title">
-              “Too Contagious to Fail”
-            </h3>
-            <div className="grid">
-              <div className="span-3 note">
+          <div className="project__intro">
+            <h1 className="project__title">Businessweek</h1>
+            <h2 className="project__subtitle">Data visualization and design</h2>
+            <div className="grid project__info">
+              <div className="span-7 body-copy">
                 <p>
-                  A diagram showing the risk each major financial instituion was exposed to by [risk] from each other. Concentrations of longer arrows indicate more counterparty risk exposure.
+                  I joined the Businessweek team to lead the research and design of visual news stories for a special issue marking the five-year anniversary of the financial meltdown triggering the Great Recession.
                 </p>
               </div>
-              <div className="span-9 align-right">
-                <img src={imgRisk} alt="Photo of 'Too Contagious to Fail' graphic"></img>
+              <div className="span-3 align-right note">
+                <p className="note__title">
+                  Team
+                </p>
+                <ul className="credit-list">
+                  <li>Editor: Josh Tyrangiel</li>  
+                  <li>Creative Director: Richard Turley</li>  
+                  <li>Design Director: Cynthia Hoffman</li>  
+                  <li>Designers and graphics editors: Rob Vargas, Allison McCann, Dorothy Gambrell, Chris Nosenzo, &amp; Evan Applegate.</li>  
+                </ul>
               </div>
             </div>
-          </section>
-          <section>
-            <h3 className="project__section-title">
-              Key index timelines
-            </h3>
-              <img src={imgTimeline} alt="Photo of key index timeline graphics"></img>
-            <div className="grid">
-              <div className="span-4 note">
-                <p>
-                  At the start of each section in the issue—one for each year sinc ethe meltdown—is a stack of graphs showing key economic indexes (GDP, household income, etc.). More of each index is revealed as the issue unfolds.
-                </p>
+          </div>
+          <div className="project__content">
+            <section>
+              <h3 className="project__section-title">
+                “Too Contagious to Fail”
+              </h3>
+              <div className="grid">
+                <div className="span-3 note">
+                  <p>
+                    A diagram showing the risk each major financial instituion was exposed to by [risk] from each other. Concentrations of longer arrows indicate more counterparty risk exposure.
+                  </p>
+                </div>
+                <div className="span-9 align-right">
+                  <img src={imgRisk} alt="Photo of 'Too Contagious to Fail' graphic"></img>
+                </div>
               </div>
-            </div>
-          </section>
-          <section>
-            <h3 className="project__section-title">
-              “World Leader Roulette”
-            </h3>
-            <div className="grid">
-              <div className="span-4 note">
-                <p>
-                  Designed with Allison McCann
-                </p>
-                <p>
-                  The purple in this heatmap shows drops in unemployment from 2007-2013, serving as the stage over which different world leaders come and go (marked by black dots). We figured eye-crops made for a more compelling set of headshots.
-                </p>
+            </section>
+            <section>
+              <h3 className="project__section-title">
+                Key index timelines
+              </h3>
+                <img src={imgTimeline} alt="Photo of key index timeline graphics"></img>
+              <div className="grid">
+                <div className="span-4 note">
+                  <p>
+                    At the start of each section in the issue—one for each year sinc ethe meltdown—is a stack of graphs showing key economic indexes (GDP, household income, etc.). More of each index is revealed as the issue unfolds.
+                  </p>
+                </div>
               </div>
-            </div>
-            <img src={imgHeads} alt="Photo of 'World Leader Roulette' graphic"></img>
-          </section>
+            </section>
+            <section>
+              <h3 className="project__section-title">
+                “World Leader Roulette”
+              </h3>
+              <div className="grid">
+                <div className="span-4 note">
+                  <p>
+                    Designed with Allison McCann
+                  </p>
+                  <p>
+                    The purple in this heatmap shows drops in unemployment from 2007-2013, serving as the stage over which different world leaders come and go (marked by black dots). We figured eye-crops made for a more compelling set of headshots.
+                  </p>
+                </div>
+              </div>
+              <img src={imgHeads} alt="Photo of 'World Leader Roulette' graphic"></img>
+            </section>
+          </div>
         </div>
         <ProjectFooter />
       </div>
--- src/work/Rapmatics/Rapmatics.js
+++ src/work/Rapmatics/Rapmatics.js
@@ -4,30 +4,99 @@ import ProjectFooter from '../../ProjectFooter';
 import '../../Project.scss';
 import './Rapmatics.scss';
 
+import imgCanIKickIt from './media/rapmatic--animated--can-i-kick-it.gif' 
+import imgGiveItToMe from './media/rapmatic--give-it-to-me-800.gif' 
+import imgWatchooWant from './media/rapmatic--watchoo-want-800.gif' 
+import imgPartyUp from './media/rapmatic--party-up-800.gif' 
+import imgTheWorldIsYours from './media/rapmatic--the-world-is-yours-800.gif' 
+import imgWorkIt from './media/rapmatic--animated--work-it.gif' 
+import imgIzzo from './media/rapmatic--animated--hova.gif' 
+import imgTheBridgeIsOver from './media/rapmatic--animated--bridge-is-over.gif' 
+import imgChildrensStory from './media/rapmatic--animated--knockem-out.gif' 
+import imgNextEpisode from './media/rapmatic--animated--next-episode.gif' 
+import imgShimmyShimmyYa from './media/rapmatic--animated--shimmy.gif' 
+
+
 function Rapmatics() {
     return (
       <div className="Project Rapmatics">
         <ProjectHeader />        
-        <section className="project__intro">
-          <div class="contained">
-            <h1 className="project__title">Rapmatics</h1>
-            <h2 className="project__subtitle">Lyric maps</h2>
-            <div class="grid">
-              <div className="span-7 body-copy">
-                <p>
-                  Music has many properties (pitch, rhythm, tempo, etc), but repetition is what makes something <em>feel</em> musical.
-                </p>
-                <p>
+        <div className="project__body">
+          <div className="project__intro">
+            <div class="contained">
+              <h1 className="project__title">Rapmatics</h1>
+              <h2 className="project__subtitle">Lyric maps</h2>
+              <div class="grid project__info">
+                <div className="span-7 body-copy">
+                  <p>
+                    Music has many properties (pitch, rhythm, tempo, etc), but repetition is what makes something <em>feel</em> musical.
+                  </p>
+                  <p>
                     These flow-mapping, loop-doodling spit-samples are an ongoing tribute to lyrical repetition in hip hop.
                   </p>
-              </div>
-              <div className="span-3 align-right note">
+                </div>
+                <div className="span-3 align-right note">
+                </div>
               </div>
             </div>
           </div>
-        </section>
-        <div className="project__body">
-          test
+          <div className="project__content">
+            <div className="rapmatic-container bg--green">
+              <div className="grid contained">
+                <img className="span-6" src={imgCanIKickIt} alt="lyric diagram for `Can I Kick It`"></img>
+              </div>
+            </div>
+            <div className="rapmatic-container bg--red">
+              <div className="grid contained">
+                <img className="span-6" src={imgGiveItToMe} alt="lyric diagram for `Give It To Me`"></img>
+              </div>
+            </div>
+            <div className="rapmatic-container bg--yellow">
+              <div className="grid contained">
+                <img className="span-6" src={imgWatchooWant} alt="lyric diagram for `So What'Cha Want`"></img>
+              </div>
+            </div>
+            <div className="rapmatic-container bg--red">
+              <div className="grid contained">
+                <img className="span-6" src={imgPartyUp} alt="lyric diagram for `Party Up`"></img>
+              </div>
+            </div>
+            <div className="rapmatic-container bg--green the-world-is-yours">
+              <div className="grid contained">
+                <img className="span-6" src={imgTheWorldIsYours} alt="lyric diagram for `The World Is Yours`"></img>
+              </div>
+            </div>
+            <div className="rapmatic-container bg--yellow">
+              <div className="grid contained">
+                <img className="span-6" src={imgWorkIt} alt="lyric diagram for `Work It`"></img>
+              </div>
+            </div>
+            <div className="rapmatic-container bg--green">
+              <div className="grid contained">
+                <img className="span-6" src={imgIzzo} alt="lyric diagram for `Izzo`"></img>
+              </div>
+            </div>
+            <div className="rapmatic-container bg--red bridge-over">
+              <div className="grid contained">
+                <img className="span-6" src={imgTheBridgeIsOver} alt="lyric diagram for `The Bridge Is Over`"></img>
+              </div>
+            </div>
+            <div className="rapmatic-container bg--yellow">
+              <div className="grid contained">
+                <img className="span-6" src={imgChildrensStory} alt="lyric diagram for `Children's Story`"></img>
+              </div>
+            </div>
+            <div className="rapmatic-container bg--green">
+              <div className="grid contained">
+                <img className="span-6" src={imgNextEpisode} alt="lyric diagram for `Next Episode`"></img>
+              </div>
+            </div>
+            <div className="rapmatic-container bg--yellow">
+              <div className="grid contained">
+                <img className="span-6" src={imgShimmyShimmyYa} alt="lyric diagram for `Shimmy Shimmy Ya`"></img>
+              </div>
+            </div>
+          </div>
         </div>
         <ProjectFooter />
       </div>
